49b50ace477a0299bc288389fd6887b6bff6c69b
Kristof <you@example.com>
2025-08-31 00:22:12 +0200

    Added editx as data source

--- editx.py
+++ editx.py
@@ -11,52 +11,56 @@ from feedgen.feed import FeedGenerator
 import json
 from yattag import Doc
 
-
-BASE_URL = 'https://www.vdab.be/vindeenjob/vacatures'
-BASE_URL_REST = 'https://www.vdab.be/rest/vindeenjob/v4/vacatures'
-PAGE_COUNT = 20
-FEED_ID = 'e2590d85-368a-4ef9-863c-931baa40fcec'
+BASE_URL = 'https://editx.eu/en/it-jobs'
+BASE_URL_REST = 'https://api.editx.eu/en/api/editx/jobs'
+BASE_URL_DETAIL_REST = 'https://api.editx.eu/en/api/editx/job'
+FEED_ID = 'd34c4e59-e6f2-4fce-8ea6-e0da265fbf22'
 
 
 def create_session() -> requests.Session:
     sess = requests.Session()
 
     sess.headers = {'Content-Type': 'application/json',
-               'Accept': 'application/json, text/plain, */*',
-               'Referer': 'https://www.vdab.be/vindeenjob/vacatures?locatie=2550%20Kontich&afstand=30&locatieCode=375&sort=standaard&diplomaNiveau=D&arbeidscircuit=8&jobdomein=JOBCAT10&arbeidsduur=V&ervaring=4&arbeidsregime=D&taal=N_15',
-               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15',
-               'Vej-Key-Monitor': 'b277002f-e1fa-4fc5-868a-fdab633c3851'
+               'Accept': 'application/json',
+               'Origin': 'https://editx.eu',
+               'Referer': 'https://editx.eu/',
+               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15'
               }
     return sess
 
 
-def get_data(config_file: Path|str, page_count: int) -> list:
+def get_data(config_file: Path|str) -> list:
     
     logging.debug('Reading configuration file...')
     with open(config_file) as f:
         config_data = json.load(f)
 
-    data = []
     sess = create_session()
     
     try:
-        for i in range(page_count):
-            logging.debug(f'Requesting data ({i}/{page_count})...')
-            config_data['pagina'] = i
-            r = sess.post('https://www.vdab.be/rest/vindeenjob/v4/vacatureLight/zoek', 
-                          data=json.dumps(config_data))
-            r.raise_for_status()
-            partial_data = r.json()['resultaten']
-            if len(partial_data) == 0:
-                logging.info(f'Bailing out early, no data received for page {i}')
-                break
-            data.extend(partial_data)
-            if i < page_count-1:
-                time.sleep(randint(3,10))
+        r = sess.get(BASE_URL_REST, 
+                     params=config_data)
+        r.raise_for_status()
+        raw_data = r.json()
+        data = raw_data['jobs']
+
+        if (total := int(raw_data['total'])) > (count := int(raw_data['count'])):
+            page_count = total % count
+            logging.debug(f'Total count is {total}, need to issue another {page_count-1} requests to fetch all data.')
+            # We already had to retrieve page 0 to figure this out,
+            # so the iterations need to start from 1
+            for i in range(1,page_count):
+                logging.debug(f'Requesting data ({i+1}/{page_count})...')
+                config_data['page'] = i
+                r = sess.get(BASE_URL_REST, 
+                            params=config_data)
+                r.raise_for_status()
+                data.extend(r.json()['jobs'])
     except Exception as e:
-        logging.error(f'Exception at attempt {i}: {e!r}. Total entries retrieved: {len(data)}.')
-    else:
-        logging.debug(f'Data retrieved successfully, {len(data)} records.')
+        logging.error(f'Failed to retrieve data: {e!r}')
+        raise
+
+    logging.debug(f'Data retrieved successfully, {len(data)} records.')
     return data
 
 
@@ -67,14 +71,14 @@ def get_posting_details(data: list) -> dict:
     sess = create_session()
     
     for i, entry in enumerate(data):
-        entry_id = entry['id']['id']
+        entry_id = entry['id']
         logging.debug(f'Retrieving details for post ID {entry_id}...')
         if entry_id in details:
             logging.warning(f'Detail data for post ID {entry_id} already exists. This should not happen!')
             continue
 
         try:
-            r = sess.get(f'{BASE_URL_REST}/{entry_id}')
+            r = sess.get(f'{BASE_URL_DETAIL_REST}/{entry_id}')
             r.raise_for_status()
             details[entry_id] = r.json()
             if i < post_count-1:
@@ -88,24 +92,32 @@ def get_posting_details(data: list) -> dict:
 
 def generate_feed(data: list, details: dict, output_file: Path|str) -> None:
 
+    def get_native_ts(ts_string):
+        try:
+            ts = datetime.strptime(ts_string, '%Y-%m-%d %H:%M:%S')
+            ts.replace(tzinfo=timezone.utc)
+        except Exception as e:
+            logging.warning(f'Failed to convert "{ts_string}" to datetime: {e!r}')
+            return None
+
     fg = FeedGenerator()
     run_timestamp = datetime.now(timezone.utc)
-    fg.title('VDAB – Zoek een job')
+    fg.title('editx')
     fg.id(FEED_ID)
-    fg.link(href='https://www.vdab.be', rel='alternate')
+    fg.link(href='https://www.editx.eu', rel='alternate')
     fg.lastBuildDate(run_timestamp)
     fg.updated(run_timestamp)
     
     for entry in data:
         fe = fg.add_entry()
-        entry_id = entry['id']['id']
+        entry_id = entry['id']
         try:
-            fe.title(f'{entry['vacaturefunctie']['naam']} ({entry['vacatureBedrijfsnaam']})')
+            fe.title(f'{entry['title']} ({entry['company']['label']})')
             link = f'{BASE_URL}/{entry_id}'
             fe.id(link)
             fe.link(href=link)
-            fe.published(entry['eerstePublicatieDatum'])
-            fe.updated(entry['laatsteWijzigingDatum'])
+            fe.published(get_native_ts(entry['onlineDate']['value']))
+            fe.updated(get_native_ts(entry['changed']['value']))
 
             detail_data = details.get(entry_id)
 
@@ -113,28 +125,42 @@ def generate_feed(data: list, details: dict, output_file: Path|str) -> None:
             doc, tag, text, line = Doc().ttl()
             with tag('p'):
                 with tag('ul'):
-                    line('li', f'Bedrijf: {entry['vacatureBedrijfsnaam']} ({entry['leverancier']['type'].lower()})')
-                    line('li', f'Locatie: {entry['tewerkstellingsLocatieRegioOfAdres'].title()}')
-                    line('li', f'Type: {entry['vacaturefunctie']['arbeidscircuitLijn']}')
+                    line('li', f'Bedrijf: {entry['company']['label']}')
+                    line('li', f'Recruiter: {entry['recruiter']['firstname']} {entry['recruiter']['lastname']}, {entry['recruiter']['position']['position']['label']} – {entry['recruiter']['position']['organization']['label']}')
+                    line('li', f'Locatie: {entry['locality']} ({entry.get('address')})')
+                    line('li', f'Published on {entry['onlineDate']['label']}, last change {entry['changed']['label']}')
             
             if detail_data is not None:
                 try:
-                    line('h3', 'Functieomschrijving')
-                    doc.asis(detail_data['functie']['omschrijving']['html'])
+                    line('h3', 'Rol')
+                    doc.asis(detail_data['role'])
                 except Exception:
                     pass
                 try:
                     line('h3', 'Profiel')
-                    doc.asis(detail_data['profiel']['vereisteKwalificaties']['html'])
+                    doc.asis(detail_data['profile'])
                 except Exception:
                     pass
                 try:
                     line('h3', 'Aanbod')
-                    doc.asis(detail_data['profiel']['aanbod']['aanbodEnVoordelen']['html'])
+                    doc.asis(detail_data['proposal'])
+                except Exception:
+                    pass
+                try:
+                    line('h3', 'Bedrijf')
+                    with tag('p'):
+                        with tag('ul'):
+                            line('li', f'Adres: {detail_data['company']['address']}')
+                            line('li', f'Vacatures: {detail_data['company']['countJobOnline']}')
+                            line('li', f'Werknemers: {detail_data['company']['numberOfEmployees']['label']}')
+                            line('li', f'Sector: {detail_data['company']['industry']['label']}')
                 except Exception:
                     pass
             
             fe.content(doc.getvalue(), type='html')
+
+            categories = [{'term': skill['label']} for skill in entry['skills']]
+            fe.category(categories)
         except Exception as e:
             logging.error(f'Failed to generate entry for post ID {entry_id}: {e!r}')
 
@@ -143,9 +169,9 @@ def generate_feed(data: list, details: dict, output_file: Path|str) -> None:
     logging.info(f'RSS feed saved.')
 
 
-def main(config_file: Path|str, output_file: Path|str, page_count: int, fetch_details: bool=False) -> None:
+def main(config_file: Path|str, output_file: Path|str, fetch_details: bool=False) -> None:
     logging.info('Retrieving data...')
-    data = get_data(config_file, page_count)
+    data = get_data(config_file)
     if fetch_details:
         logging.info('Retrieving posting details...')
         details = get_posting_details(data)
@@ -160,7 +186,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate RSS feeds for VDAB jobs.')
     parser.add_argument('output_file', help='Filename to write the RSS feed to')
     parser.add_argument('config_file', help='Filename containing the request configuration')
-    parser.add_argument('-p', '--page_count', default=PAGE_COUNT, type=int)
     parser.add_argument('-d', '--details', action='store_true', help='Enable job details')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable debug logging')
     
@@ -179,6 +204,5 @@ if __name__ == "__main__":
         exit(1)
 
     main(config_file=args.config_file, 
-         output_file=args.output_file, 
-         page_count=args.page_count, 
+         output_file=args.output_file,
          fetch_details=args.details)
---
+++ vdab.py
@@ -0,0 +1,184 @@
+import argparse
+import logging
+from pathlib import Path
+from datetime import datetime, timezone
+from random import randint
+import time
+import requests
+from requests.exceptions import HTTPError
+from feedgen.feed import FeedGenerator
+
+import json
+from yattag import Doc
+
+
+BASE_URL = 'https://www.vdab.be/vindeenjob/vacatures'
+BASE_URL_REST = 'https://www.vdab.be/rest/vindeenjob/v4/vacatures'
+PAGE_COUNT = 20
+FEED_ID = 'e2590d85-368a-4ef9-863c-931baa40fcec'
+
+
+def create_session() -> requests.Session:
+    sess = requests.Session()
+
+    sess.headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json, text/plain, */*',
+               'Referer': 'https://www.vdab.be/vindeenjob/vacatures?locatie=2550%20Kontich&afstand=30&locatieCode=375&sort=standaard&diplomaNiveau=D&arbeidscircuit=8&jobdomein=JOBCAT10&arbeidsduur=V&ervaring=4&arbeidsregime=D&taal=N_15',
+               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15',
+               'Vej-Key-Monitor': 'b277002f-e1fa-4fc5-868a-fdab633c3851'
+              }
+    return sess
+
+
+def get_data(config_file: Path|str, page_count: int) -> list:
+    
+    logging.debug('Reading configuration file...')
+    with open(config_file) as f:
+        config_data = json.load(f)
+
+    data = []
+    sess = create_session()
+    
+    try:
+        for i in range(page_count):
+            logging.debug(f'Requesting data ({i}/{page_count})...')
+            config_data['pagina'] = i
+            r = sess.post('https://www.vdab.be/rest/vindeenjob/v4/vacatureLight/zoek', 
+                          data=json.dumps(config_data))
+            r.raise_for_status()
+            partial_data = r.json()['resultaten']
+            if len(partial_data) == 0:
+                logging.info(f'Bailing out early, no data received for page {i}')
+                break
+            data.extend(partial_data)
+            if i < page_count-1:
+                time.sleep(randint(3,10))
+    except Exception as e:
+        logging.error(f'Exception at attempt {i}: {e!r}. Total entries retrieved: {len(data)}.')
+    else:
+        logging.debug(f'Data retrieved successfully, {len(data)} records.')
+    return data
+
+
+def get_posting_details(data: list) -> dict:
+    
+    details = {}
+    post_count = len(data)
+    sess = create_session()
+    
+    for i, entry in enumerate(data):
+        entry_id = entry['id']['id']
+        logging.debug(f'Retrieving details for post ID {entry_id}...')
+        if entry_id in details:
+            logging.warning(f'Detail data for post ID {entry_id} already exists. This should not happen!')
+            continue
+
+        try:
+            r = sess.get(f'{BASE_URL_REST}/{entry_id}')
+            r.raise_for_status()
+            details[entry_id] = r.json()
+            if i < post_count-1:
+                time.sleep(randint(3,10))
+        except Exception as e:
+            logging.error(f'Exception while retrieving details for post ID {entry_id}: {e!r}.')
+    
+    logging.debug(f'Retrieved {len(details)} detail record(s).')
+    return details
+
+
+def generate_feed(data: list, details: dict, output_file: Path|str) -> None:
+
+    fg = FeedGenerator()
+    run_timestamp = datetime.now(timezone.utc)
+    fg.title('VDAB')
+    fg.id(FEED_ID)
+    fg.link(href='https://www.vdab.be', rel='alternate')
+    fg.lastBuildDate(run_timestamp)
+    fg.updated(run_timestamp)
+    
+    for entry in data:
+        fe = fg.add_entry()
+        entry_id = entry['id']['id']
+        try:
+            fe.title(f'{entry['vacaturefunctie']['naam']} ({entry['vacatureBedrijfsnaam']})')
+            link = f'{BASE_URL}/{entry_id}'
+            fe.id(link)
+            fe.link(href=link)
+            fe.published(entry['eerstePublicatieDatum'])
+            fe.updated(entry['laatsteWijzigingDatum'])
+
+            detail_data = details.get(entry_id)
+
+            # Build the entry content
+            doc, tag, text, line = Doc().ttl()
+            with tag('p'):
+                with tag('ul'):
+                    line('li', f'Bedrijf: {entry['vacatureBedrijfsnaam']} ({entry['leverancier']['type'].lower()})')
+                    line('li', f'Locatie: {entry['tewerkstellingsLocatieRegioOfAdres'].title()}')
+                    line('li', f'Type: {entry['vacaturefunctie']['arbeidscircuitLijn']}')
+            
+            if detail_data is not None:
+                try:
+                    line('h3', 'Functieomschrijving')
+                    doc.asis(detail_data['functie']['omschrijving']['html'])
+                except Exception:
+                    pass
+                try:
+                    line('h3', 'Profiel')
+                    doc.asis(detail_data['profiel']['vereisteKwalificaties']['html'])
+                except Exception:
+                    pass
+                try:
+                    line('h3', 'Aanbod')
+                    doc.asis(detail_data['profiel']['aanbod']['aanbodEnVoordelen']['html'])
+                except Exception:
+                    pass
+            
+            fe.content(doc.getvalue(), type='html')
+        except Exception as e:
+            logging.error(f'Failed to generate entry for post ID {entry_id}: {e!r}')
+
+    logging.info(f'Writing to {output_file}...')
+    fg.atom_file(output_file, pretty=True)
+    logging.info(f'RSS feed saved.')
+
+
+def main(config_file: Path|str, output_file: Path|str, page_count: int, fetch_details: bool=False) -> None:
+    logging.info('Retrieving data...')
+    data = get_data(config_file, page_count)
+    if fetch_details:
+        logging.info('Retrieving posting details...')
+        details = get_posting_details(data)
+    else:
+        details = {}
+    logging.info('Building RSS feed...')
+    generate_feed(data, details, output_file)
+    logging.info('Done!')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate RSS feeds for VDAB jobs.')
+    parser.add_argument('output_file', help='Filename to write the RSS feed to')
+    parser.add_argument('config_file', help='Filename containing the request configuration')
+    parser.add_argument('-p', '--page_count', default=PAGE_COUNT, type=int)
+    parser.add_argument('-d', '--details', action='store_true', help='Enable job details')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable debug logging')
+    
+    args = parser.parse_args()
+
+    # Set up logging
+    log_level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
+    # Avoid logging from requests
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+    # Verify if configuration file exists
+    if not Path(args.config_file).is_file():
+        logging.error(f'File not found: {config_file}')
+        exit(1)
+
+    main(config_file=args.config_file, 
+         output_file=args.output_file, 
+         page_count=args.page_count, 
+         fetch_details=args.details)
